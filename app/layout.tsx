4a52deb92a68b4b25e9f9b26227b8c924a1b0f99
nishikawa <b053ffv@gmail.com>
2024-08-05 22:56:21 +0900

    #7 change: 言語設定をenからjpに変更し、フォントをInferからNoto Sans Japaneseに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_JP  } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansJP  = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="jp">
+      <body className={notoSansJP .className}>{children}</body>
     </html>
   );
 }

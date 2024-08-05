04f687fb90349c3c18a5047bae7e015e71810a23
nishikawa <b053ffv@gmail.com>
2024-08-05 22:23:04 +0900

    #2 add: Header コンポーネントの高さを画面の縦幅の20%に指定

--- app/components/day/Header.tsx
+++ app/components/day/Header.tsx
@@ -1,10 +1,8 @@
 function Header() {
   return (
-    <>
-    <div className="border border-red-500">
+    <div className="bg-red-200 h-[20vh] opacity-80">
       <h1>header</h1>
     </div>
-    </>
   );
 }
 

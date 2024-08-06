fe609e609847beaccfacbda0bdc462312a47eb53
nishikawa <b053ffv@gmail.com>
2024-08-06 18:38:08 +0900

    #2 add: 型ヒントの記述を追加

--- app/components/day/TimeLine.tsx
+++ app/components/day/TimeLine.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const TimeLine: React.FC = () => {
-  const lines = Array.from({ length: 23 }, (_, i) => `${i + 1}:00`);
+  const lines: string[] = Array.from(
+    { length: 23 },
+    (_: unknown, i: number): string => `${i + 1}:00`
+  );
 
   const textWithLine = (text: string, index: number) => {
-    const isFirst = index === 0;
-    const isLast = index === lines.length - 1;
+    const isFirst: boolean = index === 0;
+    const isLast: boolean = index === lines.length - 1;
     return (
-      <div className={`flex items-center w-full ${isFirst ? 'mt-2' : ''} ${isLast ? 'mb-2' : 'mb-1'}`}> {/* 要素の上下のパディング */}
-        <span className="w-[15vw] text-lg flex justify-center">{text}</span> 
+      <div
+        className={`flex items-center w-full ${isFirst ? "mt-2" : ""} ${
+          isLast ? "mb-2" : "mb-1"
+        }`}
+      >
+        {" "}
+        {/* 要素の上下のパディング */}
+        <span className="w-[15vw] text-lg flex justify-center">{text}</span>
         <div className="flex-grow border-t border-black"></div>
       </div>
     );
-  }
+  };
 
   return (
     <div className="flex">
       <div className="w-[20vw]">
         {lines.map((text: string, index: number) => (
-          <div key={index}>
-            {textWithLine(text, index)}
-          </div>
+          <div key={index}>{textWithLine(text, index)}</div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default TimeLine;

69446fa3b84146876bddf0dcfd253e0cba3f7688
nishikawa <b053ffv@gmail.com>
2024-08-09 01:36:21 +0900

    #2 add: ユーザーの入力データの型に従い、シフトブロックを全てのshiftLine上に表示する処理を作成

--- app/components/day/Body.tsx
+++ app/components/day/Body.tsx
@@ -2,24 +2,37 @@
 import ShiftLine from "@/app/components/day/ShiftLine";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import type { UserInputType } from "@/app/types/UserInputType";
+import { time } from "console";
+
+const testUserInput: UserInputType = {
+  name: "name",
+  selectedDevice: "Pc-1",
+  startTime: "3:00",
+  endTime: "9:00",
+  color: "red",
+};
 
 function Body() {
-  const deviceNum = 5;
-  const shiftLineHeight = useRef<HTMLDivElement | null>(null);
+  const deviceNum = 5; // 1画面に表示するShiftLineの数
+  const shiftLineHeight = useRef<HTMLDivElement | null>(null); // ShiftLineの高さを取得するためのref
+  const timeLinesRef = useRef<(HTMLDivElement | null)[]>([]); // 時刻の横線の高さを取得するためのrefの配列
 
-  const [height, setHeight] = useState(0);
-
-  useLayoutEffect(() => {
-    if (shiftLineHeight.current) {
-      setHeight(shiftLineHeight.current.offsetHeight);
-    }
-  }, []);
+  const [height, setHeight] = useState(0); // ShiftLineの高さ
 
   // 1:00 ~ 23:00 の文字列を作成
   const times: string[] = Array.from(
     { length: 23 },
     (_: unknown, i: number): string => `${i + 1}:00`
   );
+
+  useLayoutEffect(() => {
+    if (shiftLineHeight.current) {
+      setHeight(shiftLineHeight.current.offsetHeight);
+    }
+    // timeLinesRef.currentの長さをtimesの長さに合わせて初期化
+    timeLinesRef.current = timeLinesRef.current.slice(0, times.length);
+  }, [times]);
 
   // 時刻の隣に横線を描く要素を作成
   const timeWithLine = (time: string, index: number) => {
@@ -33,7 +46,12 @@ function Body() {
       >
         <span className="w-[15vw] text-lg flex justify-center">{time}</span>{" "}
         {/* 時刻 */}
-        <div className="flex-grow border-t border-black"></div>{" "}
+        <div
+          className="flex-grow border-t border-black"
+          ref={(el) => {
+            timeLinesRef.current[index] = el;
+          }}
+        ></div>{" "}
         {/* 時刻の横に引く線 */}
       </div>
     );
@@ -41,12 +59,47 @@ function Body() {
 
   // 時刻と横線の背景を作成
   const timeLines = (
-    <div className="w-[100vw] absolute z-10" ref={shiftLineHeight}> {/* timeLineの高さを shiftLine の縦幅とするために ref で取得 */}
+    <div className="w-[100vw] absolute z-10" ref={shiftLineHeight}>
+      {" "}
+      {/* timeLineの高さを shiftLine の縦幅とするために ref で取得 */}
       {times.map((time: string, index: number) => (
         <div key={index}>{timeWithLine(time, index)}</div> // 時刻と横線を表示
       ))}
     </div>
   );
+
+  // ShiftBlockの位置を計算
+  const calcBlockPosition = (startTime: string, endTime: string) => {
+    const startHour = parseInt(startTime.split(":")[0]);
+    const startMinute = parseInt(startTime.split(":")[1]);
+    const endHour = parseInt(endTime.split(":")[0]);
+    const endMinute = parseInt(endTime.split(":")[1]);
+
+    // const top = startHour * 60 + startMinute;
+    // const height = endHour * 60 + endMinute - top;
+    console.log("startHour", startHour);
+    let top:number = 0;
+    let end:number = 0;
+
+    if (
+      timeLinesRef.current[startHour - 1] !== undefined &&
+      timeLinesRef.current[startHour - 1] !== null
+    ) {
+      top = timeLinesRef.current[startHour - 1]!.offsetTop;
+    }
+    
+    if (
+      timeLinesRef.current[endHour - 1] !== undefined &&
+      timeLinesRef.current[endHour - 1] !== null
+    ) {
+      end = timeLinesRef.current[endHour - 1]!.offsetTop;
+    }
+    
+    const height = end - top;
+    
+    console.log("top", top);
+    return { top: top, left: 0, height: height };
+  };
 
   // 各ShiftLineを作成
   const shiftLines = [...Array(deviceNum)].map((_, i) => {
@@ -56,7 +109,10 @@ function Body() {
         className="w-[16vw] h-full flex-none z-20"
         style={{ height: `${height}px` }}
       >
-        <ShiftLine />
+        <ShiftLine
+          userInput={testUserInput}
+          calcBlockPosition={calcBlockPosition}
+        />
       </div>
     );
   });
--- app/components/day/ShiftLine.tsx
+++ app/components/day/ShiftLine.tsx
@@ -1,7 +1,41 @@
-function ShiftLine() {
-  return (
-    <div className="w-full h-full flex flex-col border-l border-black">
+import type { UserInputType } from "@/app/types/UserInputType";
+import { use, useEffect, useRef } from "react";
 
+interface ShiftLineProps {
+  userInput: UserInputType;
+  calcBlockPosition: (
+    startTime: string,
+    endTime: string
+  ) => { top: number; left: number; height: number };
+}
+
+function ShiftLine({ userInput, calcBlockPosition }: ShiftLineProps) {
+  const { top, left, height } = calcBlockPosition(
+    userInput.startTime,
+    userInput.endTime
+  );
+
+  const debugRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("debug: ",debugRef.current?.offsetTop);
+  }, []);
+
+  return (
+    <div className="w-full h-full flex flex-col border-l border-black relative">
+      {/* ShiftLineの仕切り線 */}
+      <div
+        className="absolute w-full"
+        style={{
+          top: `${top}px`,
+          height: `${height}px`,
+          left: `${left}px`,
+          backgroundColor: userInput.color,
+        }}
+        ref={debugRef}
+      >
+        {userInput.name}
+      </div>
     </div>
   );
 }

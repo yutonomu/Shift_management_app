56b81f503f4d8a4692d52fe467770c027e4b0f2a
nishikawa <b053ffv@gmail.com>
2024-08-10 00:26:13 +0900

    #2 シフトの新規追加フォームと編集フォームの作成（途中）

--- app/components/day/InputShiftButton.tsx
+++ app/components/day/InputShiftButton.tsx
@@ -7,26 +7,32 @@ import {
   SheetDescription,
 } from "@/components/ui/sheet";
 import InputShiftForm from "@/app/components/day/InputShiftForm";
+import Link from "next/link";
 import { PlusIcon } from "lucide-react";
 
 function InputShiftButton() {
   return (
-    <Sheet>
-      <SheetTrigger>
-        {/* デザイン案1 */}
-        {/* <button className="inline-flex items-center justify-center rounded-full bg-primary p-4 text-primary-foreground transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
-          <PlusIcon className="h-6 w-6" />
-        </button> */}
+    // <Sheet>
+    //   <SheetTrigger>
+    //     {/* デザイン案1 */}
+    //     {/* <button className="inline-flex items-center justify-center rounded-full bg-primary p-4 text-primary-foreground transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+    //       <PlusIcon className="h-6 w-6" />
+    //     </button> */}
 
-        {/* デザイン案2 */}
-        <button className="rounded-[30%] bg-gray-300 w-12 h-12 flex items-center justify-center drop-shadow-xl">
-          <PlusIcon className="text-black h-6 w-6" />
-        </button>
-      </SheetTrigger>
-      <SheetContent className="w-screen h-screen" side={"bottom"}>
-        <InputShiftForm />
-      </SheetContent>
-    </Sheet>
+    //     {/* デザイン案2 */}
+    //     <button className="rounded-[30%] bg-gray-300 w-12 h-12 flex items-center justify-center drop-shadow-xl">
+    //       <PlusIcon className="text-black h-6 w-6" />
+    //     </button>
+    //   </SheetTrigger>
+    //   <SheetContent className="w-screen h-screen" side={"bottom"}>
+    //     <InputShiftForm />
+    //   </SheetContent>
+    // </Sheet>
+   
+      <Link href={'inputShiftForm/create'} className="rounded-[30%] bg-gray-300 w-12 h-12 flex items-center justify-center drop-shadow-xl">
+        <PlusIcon className="text-black h-6 w-6" />
+      </Link>
+
   );
 }
 

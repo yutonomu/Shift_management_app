9f626f0be8aab1edf1bb5110ef44493bb3e23c13
nishikawa <b053ffv@gmail.com>
2024-08-09 18:09:23 +0900

    #2 add: InputShiftButton の位置とデザインを調整。

--- app/components/day/InputShiftButton.tsx
+++ app/components/day/InputShiftButton.tsx
@@ -8,16 +8,21 @@ import {
 } from "@/components/ui/sheet";
 import InputShiftForm from "@/app/components/day/InputShiftForm";
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
-import {Plus} from "lucide-react";
+import { Plus, PlusIcon } from "lucide-react";
 
 function InputShiftButton() {
   return (
     <Sheet>
       <SheetTrigger>
-        <Button variant="outline" size="icon">
-          <Plus className="h-4 w-4" />
-        </Button>
+        {/* デザイン案1 */}
+        {/* <button className="inline-flex items-center justify-center rounded-full bg-primary p-4 text-primary-foreground transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+          <PlusIcon className="h-6 w-6" />
+        </button> */}
+
+        {/* デザイン案2 */}
+        <button className="rounded-full bg-white border border-black w-12 h-12 flex items-center justify-center">
+          <PlusIcon className="text-black h-6 w-6" />
+        </button>
       </SheetTrigger>
       <SheetContent className="w-screen h-screen" side={"bottom"}>
         <InputShiftForm />

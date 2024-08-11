a34cd2d365a8c7a399612456037e994359e6f5eb
yutonomu <o9079821845@gmail.com>
2024-08-11 23:45:25 +0900

    #39 add:新規作成のAPIを作成した

---
+++ app/actions/postShiftAction.ts
@@ -0,0 +1,47 @@
+"use server";
+import { z } from "zod";
+import prisma from "@/lib/prisma";
+import { Devices } from "@prisma/client";
+import { getServerSession } from "next-auth/next";
+import { nextAuthOptions } from "@/lib/next-auth/options";
+
+const shiftFromSchema = z
+  .object({
+    selectedDevice: z.string().min(1, "デバイス名を入力してください"),
+    startTime: z.date(),
+    endTime: z.date(),
+  })
+  .refine((data) => data.startTime < data.endTime, {
+    message: "終了時刻は開始時刻より後にしてください",
+    path: ["endTime"],
+  });
+
+export const postShift = async ({
+  selectedDevice,
+  startTime,
+  endTime,
+}: z.infer<typeof shiftFromSchema>) => {
+  const session = await getServerSession(nextAuthOptions);
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const userId = session.user.id;
+
+  try {
+    await prisma.shift.create({
+      data: {
+        selectedDevice: selectedDevice as Devices,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Error creating shift:", error);
+  }
+};

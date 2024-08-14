a07741a03253c29f2d6ba7b09cc075ec78f0d699
nishikawa <b053ffv@gmail.com>
2024-08-14 11:28:29 +0900

    #57 fix: スマホ画面における新規シフト登録ボタンのレイアウトの修正。"新規シフト登録”の文字が消えていなかったのでスマホ画面では消えるようにした

--- app/components/day/InputShiftButton.tsx
+++ app/components/day/InputShiftButton.tsx
@@ -19,7 +19,7 @@ function InputShiftButton({
       <SheetTrigger>
         <button className="rounded-[30%] lg:rounded-sm  bg-gray-300 w-12 lg:w-auto h-12 lg:pl-2 lg:pr-2 flex items-center justify-center drop-shadow-xl lg:drop-shadow-none lg:hover:bg-white">
           <PlusIcon className="lg:hidden text-black h-6 w-6" />
-          新規シフト登録
+          <div className="hidden lg:flex">新規シフト登録</div>
         </button>
       </SheetTrigger>
       <SheetContent className="w-screen h-[80vh]" side={"bottom"}>

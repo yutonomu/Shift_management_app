ba3c9dfa94ef420aa8fbd14907d0a326d8cc2a2a
yutonomu <o9079821845@gmail.com>
2024-08-15 15:24:12 +0900

    #69 add:isOverlapShiftIdをstring[]で受け取れるように変更

--- app/types/ShiftBlockType.ts
+++ app/types/ShiftBlockType.ts
@@ -6,7 +6,7 @@ export type ShiftBlockType = {
   color: string;
   id: string;
   userId: string;
-  isOverlapShiftId: string | null; // 重複しているShiftBlockのID. 重複していない場合はnull
+  isOverlapShiftId: string[]; // 重複しているShiftBlockのID. 重複していない場合はnull
   // defaultDevice: string; // ユーザーが設定したデフォルトのデバイス名
   // project: string;
 };
---
+++ prisma/migrations/20240814143726_shift_is_overlap_change_to_json/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `isOverlapShiftId` column on the `shifts` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "shifts" DROP COLUMN "isOverlapShiftId",
+ADD COLUMN     "isOverlapShiftId" JSONB;
